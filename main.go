86b737a5e09c27d5148e7073da7fbb9e8dc06465
amirrezaask <raskarpour@gmail.com>
2020-04-24 19:40:18 +0430

    main file

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func main() {
+	studentHandler := studentHttpHandler()
+	sessionsHandler := sessionHttpHandler()
+	classesHandler := classHttpHandler()
+	teacherHandler := teacherHttpHandler()
+	http.Handle("/students", studentHandler)
+	http.Handle("/sessions", sessionsHandler)
+	http.Handle("/classes", classesHandler)
+	http.Handle("/teachers", teacherHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
